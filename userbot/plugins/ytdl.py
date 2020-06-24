78c5d56efd9285ca2aeae8d3bb8c0db891fb4a7c
toxic-dev0 <66657797+toxic-dev0@users.noreply.github.com>
2020-06-24 07:07:58 +0530

    Update ytdl.py

--- userbot/plugins/ytdl.py
+++ userbot/plugins/ytdl.py
@@ -207,12 +207,12 @@ async def download_video(v_url):
             v_url.chat_id,
             f"{ytdl_data['id']}.mp4",
             supports_streaming=True,
-            caption=ytdl_data['title'],
+            caption=" `file uploaded Successfully` \n`please join` @malayalamstatusvideospage 🌀",
             progress_callback=lambda d, t: asyncio.get_event_loop(
             ).create_task(
                 progress(d, t, v_url, c_time, "Uploading..",
                          f"{ytdl_data['title']}.mp4")))
-        os.remove(f"{ytdl_data['id']}.mp4")
+        os.remove(f"{ytdl_data['id']}.mp4"
         await v_url.delete()
         
                   
